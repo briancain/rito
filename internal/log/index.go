ea9e3f1766f6780825f0b12f12dba1d7604fb5f4
Brian Cain <bcain@hashicorp.com>
2023-04-11 08:53:47 -0700

    Add new index funcs, add basic Config for segments of a file

--- internal/log/index.go
+++ internal/log/index.go
@@ -17,3 +17,45 @@ type index struct {
 	mmap gommap.MMap
 	size uint64
 }
+
+func newIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	idx.size = uint64(fi.Size())
+	if err = os.Truncate(
+		f.Name(), int64(c.Segment.MaxIndexBytes),
+	); err != nil {
+		return nil, err
+	}
+
+	if idx.mmap, err = gommap.Map(
+		idx.file.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	); err != nil {
+		return nil, err
+	}
+	return idx, nil
+}
+
+// Close ensures the memory mapped file has synced its data to the persisted
+// file and that the file has flushed its contents to stable storage
+func (i *index) Close() error {
+	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
+		return err
+	}
+	if err := i.file.Sync(); err != nil {
+		return err
+	}
+	if err := i.file.Truncate(int64(i.size)); err != nil {
+		return err
+	}
+	return i.file.Close()
+}
