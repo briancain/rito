f6bd3294bd7b3c038755c48b8f92a8448c2f2236
Brian Cain <bcain@hashicorp.com>
2023-04-12 13:27:33 -0700

    auth: Add initial authorizer using casbin

--- internal/auth/authorizer.go
+++ internal/auth/authorizer.go
@@ -1,1 +1,16 @@
 package auth
+
+import (
+	"github.com/casbin/casbin"
+)
+
+type Authorizer struct {
+	enforcer *casbin.Enforcer
+}
+
+func New(model, policy string) *Authorizer {
+	e := casbin.NewEnforcer(model, policy)
+	return &Authorizer{
+		enforcer: e,
+	}
+}
