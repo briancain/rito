aaba3451040a464266f2395913505586e592660b
Brian Cain <bcain@hashicorp.com>
2023-04-12 08:58:15 -0700

    Add produce and consume stream endpoints

--- internal/server/server.go
+++ internal/server/server.go
@@ -53,3 +53,49 @@ func (s *grpcServer) Consume(
 
 	return &api.ConsumeResponse{Record: record}, nil
 }
+
+// ProduceStream implements bidirectional streaming. This enables a client
+// to stream data into the servers log and the server tells the client whether each
+// request succeeded
+func (s *grpcServer) ProduceStream(stream api.Log_ProduceStreamServer) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		res, err := s.Produce(stream.Context(), req)
+		if err != nil {
+			return err
+		}
+		if err = stream.Send(res); err != nil {
+			return err
+		}
+	}
+}
+
+// ConsumeStream implements a server-side streaming so the client can tell the
+// server where in the log to read records
+func (s *grpcServer) ConsumeStream(
+	req *api.ConsumeRequest,
+	stream api.Log_ConsumeStreamServer,
+) error {
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		default:
+			res, err := s.Consume(stream.Context(), req)
+			switch err.(type) {
+			case nil:
+			case api.ErrOffsetOutOfRange:
+				continue
+			default:
+				return err
+			}
+			if err = stream.Send(res); err != nil {
+				return err
+			}
+			req.Offset++
+		}
+	}
+}
