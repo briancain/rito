05e8eb795173ad738a9e76b33f9e93853ba8b389
Brian Cain <bcain@hashicorp.com>
2023-04-13 11:42:29 -0700

    log: Add initial log replicator

---
+++ internal/log/replicator.go
@@ -0,0 +1,22 @@
+package log
+
+import (
+	"sync"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+
+	api "github.com/briancain/rito/api/v1"
+)
+
+type Replicator struct {
+	DialOptions []grpc.DialOption
+	LocalServer api.LogClient
+
+	logger *zap.Logger
+
+	mu      sync.Mutex
+	servers map[string]chan struct{}
+	closed  bool
+	close   chan struct{}
+}
